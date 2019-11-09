a3ce2c477c6f3a714a67625eb26a87df3b95af6e
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-09 21:01:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,11 +3,11 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-
+  attr_accessor :email_addresses
 
   def parse
+    
   end
-
 
 
 end
